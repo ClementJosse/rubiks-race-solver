eef41515b1e7c69a62fde2838a3fcbaf4c4c8559
ClementJosse <clement.b.josse@gmail.com>
2023-05-20 02:10:26 +0200

    add full ida* solution, but program loops on a local minimum heuristic value

---
+++ src/main/rubixrace/BoardState.java
@@ -0,0 +1,43 @@
+package rubixrace;
+
+public class BoardSate {
+    private Tile[][] board = new Tile[5][5];
+    private String historic;
+    public int void_tile_i;
+    public int void_tile_j;
+    BoardSate(Tile[][] board, String historic, char nextStep, int void_tile_i, int void_tile_j){
+        copyBoardState(board);
+        this.historic=historic+nextStep;
+        this.void_tile_i = void_tile_i;
+        this.void_tile_j = void_tile_j;
+    }
+
+    private void copyBoardState(Tile[][] modelBoard) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                board[i][j]=new Tile(modelBoard[i][j].getTileColor());
+            }
+        }
+    }
+
+    public Tile[][] getBoard() {
+        return board;
+    }
+
+    public void swapVoidTile(int i, int j){
+        Tile temp=new Tile(board[void_tile_i][void_tile_j].getTileColor());
+        board[void_tile_i][void_tile_j]=board[void_tile_i+i][void_tile_j+j];
+        board[void_tile_i+i][void_tile_j+j]= temp;
+        void_tile_i=void_tile_i+i;
+        void_tile_j=void_tile_j+j;
+
+    }
+
+    public String getHistoric() {
+        return historic;
+    }
+
+    public char getPreviousStep(){
+        return historic.charAt(historic.length()-1);
+    }
+}
